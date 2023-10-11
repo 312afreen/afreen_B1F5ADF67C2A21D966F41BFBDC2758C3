e5aaa116b7b94537a7f347c328e2a691b05e1bc9
312afreen <147573022+312afreen@users.noreply.github.com>
2023-10-11 04:56:53 -0700

    Add files via upload

---
+++ chellenge1/main (1).py
@@ -0,0 +1,23 @@
+#Write a program that determines whether a year entered by the user is a leap year or not using if-elif-else statements.
+''''''
+year%4==0&
+
+year%100!=0/
+
+year%400==0
+''''''
+def is Leap Year(year):
+
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+
+return True
+
+else:
+
+return False
+
+year = int(input("Enter a year: "))
+
+if isLeap Year(year): # Call the function and pass the 'year' variable print('{} is a leap year.'.format(year)) else:
+
+print('{} is not a leap year..format(year))
